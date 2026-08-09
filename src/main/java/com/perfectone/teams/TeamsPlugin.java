9d2e8ed9d87b8b8cc3637d4c1444fa99032c85d9
AYSE <tomtv9889@gmail.com>
2026-08-09 19:16:27 +0530

    Add files via upload

---
+++ src/main/java/com/perfectone/teams/TeamsPlugin.java
@@ -0,0 +1,85 @@
+package com.perfectone.teams;
+
+import com.perfectone.teams.bridge.DiscordBridge;
+import com.perfectone.teams.commands.BountyCommand;
+import com.perfectone.teams.commands.LeaderboardCommand;
+import com.perfectone.teams.commands.TeamCommand;
+import com.perfectone.teams.data.Database;
+import com.perfectone.teams.listeners.ChatListener;
+import com.perfectone.teams.listeners.PlayerDeathListener;
+import com.perfectone.teams.listeners.PlayerJoinQuitListener;
+import com.perfectone.teams.manager.PlayerDataManager;
+import com.perfectone.teams.manager.TeamManager;
+import com.perfectone.teams.util.BannedNameManager;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TeamsPlugin extends JavaPlugin {
+
+    private Database database;
+    private TeamManager teamManager;
+    private PlayerDataManager playerDataManager;
+    private DiscordBridge discordBridge;
+    private BannedNameManager bannedNameManager;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        String dbFile = getConfig().getString("storage.database-file", "teams.db");
+        database = new Database(getDataFolder(), dbFile, getLogger());
+        database.connect();
+
+        teamManager = new TeamManager(this, database);
+        playerDataManager = new PlayerDataManager(database);
+        discordBridge = new DiscordBridge(this);
+        bannedNameManager = new BannedNameManager(this);
+        bannedNameManager.loadOrCreate();
+
+        // Load synchronously at startup - fast for SQLite on a local file.
+        teamManager.loadAll();
+        playerDataManager.loadAll();
+
+        getCommand("team").setExecutor(new TeamCommand(this));
+        getCommand("leaderboard").setExecutor(new LeaderboardCommand(this));
+        getCommand("bounty").setExecutor(new BountyCommand(this));
+
+        Bukkit.getPluginManager().registerEvents(new PlayerDeathListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerJoinQuitListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new ChatListener(this), this);
+
+        if (discordBridge.isEnabled()) {
+            long interval = getConfig().getLong("discord-bridge.sync-interval-seconds", 60) * 20L;
+            Bukkit.getScheduler().runTaskTimer(this, () -> discordBridge.pushSnapshotAsync(), 100L, interval);
+        }
+
+        getLogger().info("PerfectTeams enabled - " + teamManager.all().size() + " teams loaded.");
+    }
+
+    @Override
+    public void onDisable() {
+        if (database != null) {
+            database.disconnect();
+        }
+    }
+
+    public TeamManager getTeamManager() {
+        return teamManager;
+    }
+
+    public PlayerDataManager getPlayerDataManager() {
+        return playerDataManager;
+    }
+
+    public Database getDatabase() {
+        return database;
+    }
+
+    public DiscordBridge getDiscordBridge() {
+        return discordBridge;
+    }
+
+    public BannedNameManager getBannedNameManager() {
+        return bannedNameManager;
+    }
+}
